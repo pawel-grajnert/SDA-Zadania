654e51fd4d7f9b5813c9f40e531d0e19b921f796
Paweł Grajnert <pt.grajnert@gmail.com>
2023-06-16 10:40:57 +0200

    Exercise solution

---
+++ FizzBuzzAlgorithm/FizzBuzzAlgorithm.Core/FizzBuzzAlgorithm.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+namespace FizzBuzzAlgorithm.Core;
+
+public class FizzBuzzAlgorithm
+{
+    public string ProcessNumber(int number)
+    {
+        StringBuilder resultBuilder = new();
+
+        if (number % 3 == 0)
+        {
+            resultBuilder.Append("fizz");
+        }
+        
+        if (number % 5 == 0)
+        {
+            resultBuilder.Append("buzz");
+        }
+
+        if (resultBuilder.Length == 0)
+        {
+            resultBuilder.Append($"{number}");
+        }
+
+        return resultBuilder.ToString();
+    }
+}
