cebfa474f6f461582f60f3af08598106ffcc01cb
Paweł Grajnert <pt.grajnert@gmail.com>
2023-06-23 10:39:14 +0200

    Exercise Solution

---
+++ BenchmarkTest/BenchmarkTest/StringAnalyzer.cs
@@ -0,0 +1,31 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
+using System.Text.RegularExpressions;
+
+namespace BenchmarkTest;
+
+[SimpleJob(RunStrategy.ColdStart, iterationCount: 1000)]
+[MinColumn, MaxColumn, MeanColumn, MedianColumn]
+public class StringAnalyzer
+{
+    [Benchmark]
+    [Arguments("1234567890")]
+    public bool IsStringNumberRegex(string value)
+    {
+        return Regex.IsMatch(value, @"^\d$");
+    }
+
+    [Benchmark]
+    [Arguments("1234567890")]
+    public bool IsStringNumberParse(string value)
+    {
+        return long.TryParse(value, out long number);
+    }
+
+    [Benchmark]
+    [Arguments("1234567890")]
+    public bool IsStringNumberLinq(string value)
+    {
+        return value.All(char.IsDigit);
+    }
+}
